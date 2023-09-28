7dbed2a2c787a3aeff270b3efa871023aff80199
Alex Yatsun <starcrafter4444@gmail.com>
2023-09-28 22:57:13 +0300

    Set urls

--- spend_revenue/urls.py
+++ spend_revenue/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
+from revenue.views import revenue_statistics
+from spend.views import spend_statistics
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/revenue/statistics/", revenue_statistics),
+    path("api/spend/statistics/", spend_statistics),
 ]
