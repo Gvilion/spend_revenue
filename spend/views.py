cd5e5b2045c658b0817eed48896b0fa24ad54fa1
Alex Yatsun <starcrafter4444@gmail.com>
2023-09-28 22:56:58 +0300

    Create spend view

--- spend/views.py
+++ spend/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from _decimal import Decimal
 
-# Create your views here.
+from django.db.models import Sum
+from django.db.models.functions import Coalesce
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import SpendStatistic
+
+
+@api_view(['GET'])
+def spend_statistics(request):
+    spend_data = SpendStatistic.objects.values('date', 'name').annotate(
+        total_spend=Sum('spend'),
+        total_impressions=Sum('impressions'),
+        total_clicks=Sum('clicks'),
+        total_conversion=Sum('conversion'),
+        total_revenue=Sum('revenuestatistic__revenue'),
+    ).annotate(
+        total_revenue=Coalesce("total_revenue", Decimal(0)),
+    )
+
+    spend_list = list(spend_data)
+
+    return Response(spend_list)
